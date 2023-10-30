9bebd4dafa5e99738d8bcc1fe74b503a4444e0e8
MrSallivan <nik.sallivan@gmail.com>
2023-10-30 23:49:17 +1000

    модальное окно

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import './index.scss';
+import React, { useState } from "react"
+import "./index.scss"
 
-function App() {
+const Modal = ({ open, setOpen, children }) => {
   return (
-    <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+    <div className={`overlay animated ${open ? " show" : ""}`}>
+      <div className="modal">
+        <svg
+          onClick={() => setOpen((prevState) => !prevState)}
+          height="200"
+          viewBox="0 0 200 200"
+          width="200"
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const [open, setOpen] = useState(false)
+  return (
+    <div className="App">
+      <button
+        onClick={() => setOpen((prevState) => !prevState)}
+        className="open-modal-btn"
+      >
+        ✨ Открыть окно
+      </button>
+      {/* Вариант 1 */}
+      {/* {open && (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              onClick={() => setOpen((prevState) => !prevState)}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      )} */}
+      {/* Вариант 2 */}
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>It is Modal window</h3>
+      </Modal>
+    </div>
+  )
+}
+
+export default App
