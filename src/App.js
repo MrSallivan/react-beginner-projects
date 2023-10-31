55287aa02d50e4c7200d086efb919fb052f9ad9c
MrSallivan <nik.sallivan@gmail.com>
2023-10-31 23:18:48 +1000

    end squiz

--- src/App.js
+++ src/App.js
@@ -24,40 +24,68 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
     </div>
-  );
+  )
 }
 
-function Game() {
+function Game({ step, question, setStep, handleClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100)
   return (
     <>
       <div className="progress">
-        <div style={{ width: "10%" }} className="progress__inner"></div>
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((quest, index) => (
+          <li key={quest} onClick={() => handleClickVariant(index)}>
+            {quest}
+          </li>
+        ))}
       </ul>
     </>
   )
 }
 
 function App() {
-	const [step, setStep] = useState(0)
+  const [step, setStep] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const question = questions[step]
+
+  const handleClickVariant = (variant) => {
+    setStep(step + 1)
+    if (variant === question.correct) {
+      setCorrect(correct + 1)
+    }
+  }
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game
+          step={step}
+          question={question}
+          setStep={setStep}
+          handleClickVariant={handleClickVariant}
+        />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
