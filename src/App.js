a93783f8a3b2f5d89c1fc33cf53bb59a2ffbfe64
MrSallivan <nik.sallivan@gmail.com>
2023-11-18 23:17:25 +1000

    end currency board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Block } from "./Block"
 import "./index.scss"
 
 function App() {
-  const [rates, setRates] = useState({})
-  const [fromCurrency, setFromCurrency] = useState("USD")
-  const [toCurrency, setToCurrency] = useState("RUB")
+  const [fromCurrency, setFromCurrency] = useState("RUB")
+  const [toCurrency, setToCurrency] = useState("USD")
   const [fromPrice, setFromPrice] = useState(0)
-  const [toPrice, setToPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(1)
+  // const [rates, setRates] = useState({ USDUSD: 1 })
+  const ratesRef = useRef({})
 
   useEffect(() => {
     var myHeaders = new Headers()
     myHeaders.append("apikey", "S2vIv5tp5Ce2wHRiS5DNpuBLFvAUy65A")
-
     var requestOptions = {
       method: "GET",
       redirect: "follow",
@@ -23,22 +23,41 @@ function App() {
       requestOptions
     )
       .then((response) => response.json())
-      .then((result) => setRates(result["quotes"]))
+      .then((result) => {
+        ratesRef.current = result["quotes"]
+        ratesRef.current["USDUSD"] = 1
+        onChangeToPrice(1)
+      })
       .catch((error) => console.log("error", error))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const onChangeFromPrice = (value) => {
-			const cur = Object.keys(rates).find((itemCur)=>itemCur.includes(fromCurrency)) 
-			console.log(cur)
-      const price = value / rates[cur]
-      const result = price * rates[`USD${toCurrency}`]
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fromCurrency])
 
-      setToPrice(result)
-      setFromPrice(value)
-   
+  React.useEffect(() => {
+    onChangeToPrice(toPrice)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toCurrency])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function onChangeFromPrice(value) {
+    const price = value / ratesRef.current[`USD${fromCurrency}`]
+    const result = price * ratesRef.current[`USD${toCurrency}`]
+    setToPrice(result.toFixed(3))
+    setFromPrice(value)
   }
-  const onChangeToPrice = (value) => {
-  
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function onChangeToPrice(value) {
+    const result =
+      (ratesRef.current[`USD${fromCurrency}`] /
+        ratesRef.current[`USD${toCurrency}`]) *
+      value
+    setFromPrice(result.toFixed(3))
+    setToPrice(value)
   }
 
   return (
