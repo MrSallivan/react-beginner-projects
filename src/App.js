cb4aca197b78cb2cea8b03b55044eee5e28d1cbc
MrSallivan <nik.sallivan@gmail.com>
2023-11-21 23:26:34 +1000

    get collections and render collection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import './index.scss';
-
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+import React, { useEffect, useState } from "react"
+import Collection from "./Collection"
+import "./index.scss"
 
 function App() {
+  const [collections, setCollections] = useState([])
+
+  useEffect(() => {
+    fetch("https://655b5afcab37729791a8f7f5.mockapi.io/photos")
+      .then((res) => res.json())
+      .then((data) => setCollections(data))
+      .catch((err) => {
+        console.warn("Ошибка")
+        alert("Ошибка при получении данных")
+      })
+  }, [])
+
+  console.log(collections)
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -30,23 +31,17 @@ function App() {
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {collections.map((collect, index) => (
+          <Collection key={index} name={collect.name} images={collect.photos} />
+        ))}
       </div>
       <ul className="pagination">
-        <li>1</li>
+        <li>1</li> 
         <li className="active">2</li>
         <li>3</li>
       </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
