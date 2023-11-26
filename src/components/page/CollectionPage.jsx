c52b045bdb543b2e951c39dc5c007d6d45ddd240
MrSallivan <nik.sallivan@gmail.com>
2023-11-27 00:48:10 +1000

    начать попап

--- src/components/page/CollectionPage.jsx
+++ src/components/page/CollectionPage.jsx
@@ -11,12 +11,21 @@ const CollectionPage = ({ idCollection, collectionAll }) => {
       <p className="gallery__title">
         {collectionAll.filter((item) => item.id == idCollection)[0]["name"]}
       </p>
-      <div className="pictures">
-        {collectionAll
-          .filter((item) => item.id == idCollection)[0]
-          ["photos"].map((image, i) => (
-            <img key={i} className="pic" src={image} alt="pictires" />
-          ))}
+      <div className="gallery__content">
+        <div className="gallery__items">
+          {collectionAll
+            .filter((item) => item.id == idCollection)[0]
+            ["photos"].map((image, i) => (
+              <div key={i} className="gallery__item">
+                <img
+                  key={i}
+                  className="gallery__img"
+                  src={image}
+                  alt="pictires"
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   )
